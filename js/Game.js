b5d8de70ed0fe30903accd68c5a122b05b740fb4
mazei513 <mazei513@gmail.com>
2016-10-05 02:43:40 +0800

    Station and StationMarket Initial Work
    Started Station object and StationMarket class implementation.
    Added arrayCopy function to clone/copy arrays. Needed due to needing to
    copy/clone an array of objects
    StationMarketList is the list that holds all the stations implemented in
    the game. Used as a master list to populate the StationMarket deck.
    
    Station : The struct for a station card
    
    StationMarket : Class that performs the functions of the station market.
    This includes creating the deck of stations and manipulating the deck,
    such as drawing cards and populating the deck.

--- js/Game.js
+++ js/Game.js
@@ -28,3 +28,46 @@ function Player(name) {
 	this.money = 50;
 }
 /***********************************/
+
+// Custom array copy function needed due to having array of objects
+function arrayCopy(o) {
+   var output, v, key;
+   output = Array.isArray(o) ? [] : {};
+   for (key in o) {
+       v = o[key];
+       output[key] = (typeof v === "object") ? copy(v) : v;
+   }
+   return output;
+}
+
+// Station object
+function Station(price, upkeepResource, upkeepCost, production) {
+	this.price = price;
+	this.upkeepResource = upkeepResource;
+	this.upkeepCost = upkeepCost;
+	this.production = production;
+}
+
+// Global StationMarket List
+// Placeholder example stations entered in list
+var StationMarketList = [new Station(3, 'Noodle', 2, 1), new Station(4, 'Rice', 2, 1)];
+
+// StationMarket class
+class StationMarket {
+	constructor() {
+		this.StationDeck = arrayCopy(StationMarketList);
+	}
+	
+	restartDeck() {
+		this.StationDeck = arrayCopy(StationMarketList);
+	}
+	
+	drawCard() {
+		var card = Math.floor(Math.random(this.StationDeck.length));
+		return this.StationDeck.splice(card, 1);
+	}
+	
+	deckCount() {
+		return this.StationDeck.length;
+	}
+}
