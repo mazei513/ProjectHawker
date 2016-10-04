adc16b8418980c29d578eff1d55f26c5907ff7f0
mazei513 <mazei513@gmail.com>
2016-10-05 03:04:57 +0800

    Station and StationMarket update 0.1
    Extended StationMarketLIst for testing purposes
    Added currentMarket and futureMarket to StationMarket class
    Fixed error in deck drawing rng calculation

--- js/Game.js
+++ js/Game.js
@@ -35,7 +35,7 @@ function arrayCopy(o) {
    output = Array.isArray(o) ? [] : {};
    for (key in o) {
        v = o[key];
-       output[key] = (typeof v === "object") ? copy(v) : v;
+       output[key] = (typeof v === "object") ? arrayCopy(v) : v;
    }
    return output;
 }
@@ -50,20 +50,37 @@ function Station(price, upkeepResource, upkeepCost, production) {
 
 // Global StationMarket List
 // Placeholder example stations entered in list
-var StationMarketList = [new Station(3, 'Noodle', 2, 1), new Station(4, 'Rice', 2, 1)];
+var StationMarketList = [	new Station(3, 'Noodle', 2, 1),
+							new Station(4, 'Rice', 2, 1),
+							new Station(5, 'Chicken', 1, 1),
+							new Station(6, 'Tofu', 4, 1),
+							new Station(7, 'Rice', 4, 2),
+							new Station(8, 'Chicken', 3, 2),
+							new Station(9, 'Tofu', 3, 1),
+							new Station(10, 'Noodle', 4, 2),
+							new Station(11, 'Water', 0, 1)
+							];
 
 // StationMarket class
 class StationMarket {
 	constructor() {
 		this.StationDeck = arrayCopy(StationMarketList);
+		this.currentMarket = this.StationDeck.splice(0, 4);
+		this.futureMarket = this.StationDeck.splice(0, 4);
 	}
 	
+	// Resets the deck
 	restartDeck() {
+		this.StationDeck = [];
+		this.currentMarket = [];
+		this.futureMarket = [];
 		this.StationDeck = arrayCopy(StationMarketList);
+		this.currentMarket = this.StationDeck.splice(0, 4);
+		this.futureMarket = this.StationDeck.splice(0, 4);
 	}
 	
 	drawCard() {
-		var card = Math.floor(Math.random(this.StationDeck.length));
+		var card = Math.floor(Math.random()*this.StationDeck.length);
 		return this.StationDeck.splice(card, 1);
 	}
 	
